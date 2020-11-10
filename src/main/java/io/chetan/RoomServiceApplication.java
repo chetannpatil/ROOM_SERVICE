8782d4d6182650d1b21bb0e8de1f881d536ba0cf
CHETAN PATIL <chetu.is@gmail.com>
2020-11-10 20:09:35 +0530

    imlemetend ribbon,sleuth and zipkin

--- src/main/java/io/chetan/RoomServiceApplication.java
+++ src/main/java/io/chetan/RoomServiceApplication.java
@@ -1,12 +1,21 @@
 package io.chetan;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RoomServiceApplication {
 
-	public static void main(String[] args) {
+	private static Logger LOGGER = LogManager.getLogger(RoomServiceApplication.class);
+	
+	public static void main(String[] args) 
+	{
+		LOGGER.info("\n\n RoomServiceApplication - main\n\n");
+		
 		SpringApplication.run(RoomServiceApplication.class, args);
 	}
 
